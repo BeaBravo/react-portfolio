b5c7954e080dec7fdc1cdf828adfa4a71953e159
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-04 13:22:55 -0700

    added form for contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,54 @@
+import { useState } from "react";
 export default function Contact() {
+    // Here we set two state variables for firstName and lastName using `useState`
+    const [userName, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('')
+
+
+    const handleFormSubmit = (e) => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        e.preventDefault();
+    };
+
     return (
-        <div>
-            <h1>Contact Page</h1>
-            <p>
-                Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-                molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-                magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-                efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-                mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-                posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-                faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-                ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-                dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-                conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-                rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-            </p>
+        <div className="mt-5 container">
+            <h1>
+                Contact Me
+            </h1>
+            <form className="form" onSubmit={handleFormSubmit}>
+                <div className="form-group my-4 row">
+                    <label >Name:</label>
+                    <input
+                        className="col-md-7 col-11 mx-2"
+                        name="name"
+                        type="text"
+                        placeholder="your name here"
+                    />
+                </div>
+                <div className="form-group my-4 row">
+                    <label>Email: </label>
+                    <input
+                        className="col-md-7 col-11 mx-2"
+                        name="email"
+                        type="email"
+                        placeholder="youremail@email.com"
+                    />
+                </div>
+                <div className="form-group my-4 row">
+                    <label>Message: </label>
+                    <textarea
+                        className="col-md-7 col-11 mx-2"
+                        name="message"
+                        type="text"
+                        placeholder=""
+                        rows='6'
+                    />
+                </div>
+                <button className='px-2' type="submit">
+                    Submit
+                </button>
+            </form>
         </div>
     );
 }
