8d6563a71eba79508e9a8da319a1f082c2333f6a
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-03 17:27:32 -0700

    added router provide to be able to switch pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,26 @@ import { Link, useLocation } from 'react-router-dom'
 
 function Header() {
     //will have nav and title
-    return <header>this is the header </header>
+
+    // const currentPage = useLocation().pathname;
+
+    return (<header><h1>Beatriz Bravo</h1>
+        <ul className='nav'>
+            <li className='nav-item'>
+                About Me
+            </li>
+            <li className='nav-item'>
+                Portfolio
+            </li>
+            <li className='nav-item'>
+                Contact Me
+            </li>
+            <li className='nav-item'>
+                Resume
+            </li>
+        </ul>
+    </header>
+    );
 }
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,41 @@
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
+import Error from './pages/Error.jsx'
+import About from './pages/About.jsx'
+import Portfolio from './pages/Portfolio.jsx'
+import Contact from './pages/Contact.jsx'
+import Resume from './pages/Resume.jsx'
+
 // import css
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <About />
+            },
+            {
+                path: '/Portfolio',
+                element: <Portfolio />
+            },
+            {
+                path: '/Contact',
+                element: <Contact />
+            },
+            {
+                path: '/Resume',
+                element: <Resume />
+            }
+        ]
+    }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+    <RouterProvider router={router} />
 )
