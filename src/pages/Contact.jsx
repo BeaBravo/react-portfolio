85870cc15a24d2170f77996145aac438dd553aa4
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-04 13:58:40 -0700

    form validates email and message content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
 export default function Contact() {
     // Here we set two state variables for firstName and lastName using `useState`
-    const [userName, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
 
+    const handleEmail = (e) => {
+        const { name, value } = e.target
+        //check value of the email with regex 
+        const regex = new RegExp("^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$")
+        //if not valid, it will display an error message
+        regex.test(value) ? setErrorMessage('') : setErrorMessage('Please enter a valid email address');
+
+        return
+    }
+
+    const handleMessage = (e) => {
+        const { name, value } = e.target
+        //if message is empty, it will display an error message
+        value === '' ? setErrorMessage('message cannot be empty') : setErrorMessage('')
+    }
 
     const handleFormSubmit = (e) => {
         // Preventing the default behavior of the form submit (which is to refresh the page)
@@ -32,12 +45,14 @@ export default function Contact() {
                         className="col-md-7 col-11 mx-2"
                         name="email"
                         type="email"
+                        onChange={handleEmail}
                         placeholder="youremail@email.com"
                     />
                 </div>
                 <div className="form-group my-4 row">
                     <label>Message: </label>
                     <textarea
+                        onChange={handleMessage}
                         className="col-md-7 col-11 mx-2"
                         name="message"
                         type="text"
@@ -45,6 +60,7 @@ export default function Contact() {
                         rows='6'
                     />
                 </div>
+                <p>{errorMessage}</p>
                 <button className='px-2' type="submit">
                     Submit
                 </button>
