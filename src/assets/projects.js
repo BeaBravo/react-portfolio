0aaa70d8b3768df3392a56d6ab93539ae8ebd9f2
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-04 12:51:15 -0700

    passing project object as props

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -5,28 +5,28 @@ const projectList = [
         image: project1,
         title: "Place-holder1",
         link: "https://www.github.com/BeaBravo",
-        techUse: "React"
+        techUsed: "React"
     },
     {
         id: 2,
         image: project1,
         title: "Place-holder2",
         link: "github.com/BeaBravo",
-        techUse: "React"
+        techUsed: "React"
     },
     {
         id: 3,
         image: project1,
         title: "Place-holder3",
         link: "github.com/BeaBravo",
-        techUse: "React"
+        techUsed: "React"
     },
     {
         id: 4,
         image: project1,
         title: "Place-holder4",
         link: "github.com/BeaBravo",
-        techUse: "React"
+        techUsed: "React"
     },
 ]
 
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,7 +8,7 @@ function Portfolio() {
         <div id='portfolio'>
             <h1>Portfolio</h1>
             <section className="projects row justify-content-evenly">
-                {projectList.map((project) => <Project title={project.title} image={project.image} link={project.link} techUsed={project.techUse} />)}
+                {projectList.map((project) => <Project {...project} />)}
             </section>
         </div>
     );
