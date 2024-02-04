e6f96f1d5466b4408fba8e794acb76afe87a1a82
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-04 12:26:27 -0700

    added icons with links to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,19 @@
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 export default function Footer() {
-    return <footer>Made with love and React</footer>
+    return (<footer>
+        <Link className="mx-4"
+            to='https://www.github.com/BeaBravo'
+        >
+            <BsGithub />
+        </Link>
+        <t />
+        <Link
+            to='https://www.linkedin.com/in/beatriz-bravo/'
+        >
+            <BsLinkedin />
+        </Link>
+    </footer>
+    );
 }
