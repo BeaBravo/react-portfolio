6a065a498fa8fc5b0a36c5b778951cdd72a75bed
Beatriz Bravo <beatz.bravo@gmail.com>
2024-02-04 14:22:16 -0700

    added hover for icon and link

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,8 +6,10 @@ import { BsGithub } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 function Project({ functionality, title, link, techUsed, image }) {
     const [hover, setHover] = useState(false);
+    const [hoverTitle, setHoverTitle] = useState(false);
+    const [hoverIcon, setHoverIcon] = useState(false);
     return (
-        <div className={hover ? "project col-xs-10 col-sm-8 col-md-5 my-5 col-lg-5 p-3 opacity-75 border-2" : "project col-xs-10 col-sm-8 col-md-5 my-5 col-lg-5 p-3"}
+        <div className={hover ? "project col-xs-10 col-sm-8 col-md-5 my-5 col-lg-5 p-3 opacity-75 border-5" : "project col-xs-10 col-sm-8 col-md-5 my-5 col-lg-5 p-3"}
             onMouseEnter={() => setHover(true)}
             onMouseLeave={() => setHover(false)}
             style={{ backgroundImage: `url(${image})`, backgroundSize: "100% 100%", minHeight: "250px", height: "auto" }} >
@@ -16,14 +18,20 @@ function Project({ functionality, title, link, techUsed, image }) {
                     className='my-4'
                     to={link}
                     target='_blank'
-                    style={{ textDecoration: "none", color: "#9a275a", fontWeight: "bold", fontSize: "larger" }}>
+                    style={hoverTitle ? { textDecoration: "none", color: "black", fontWeight: "bold", fontSize: "larger", opacity: '1' } :
+                        { textDecoration: "none", color: "#9a275a", fontWeight: "bold", fontSize: "larger", opacity: '1' }}
+                    onMouseEnter={() => setHoverTitle(true)}
+                    onMouseLeave={() => setHoverTitle(false)}>
                     {title}
                 </Link>
                 <Link
                     className='mx-3 mb-2'
                     to={link}
                     target='_blank'
-                    style={{ textDecoration: "none" }}><BsGithub style={{ color: "#9a275a", marginBottom: "1rem", fontSize: "larger" }} />
+                    style={{ textDecoration: "none" }}><BsGithub style={hoverIcon ? { color: "black", marginBottom: "1rem", fontSize: "larger" } :
+                        { color: "#9a275a", marginBottom: "1rem", fontSize: "larger" }}
+                        onMouseEnter={() => setHoverIcon(true)}
+                        onMouseLeave={() => setHoverIcon(false)} />
                 </Link>
                 <p className='my-4'
                     style={{ color: "#9a275a", fontWeight: "lighter" }}>{techUsed}</p>
