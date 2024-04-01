b82f47fd88c087d2f707302247328ea28114f92d
Beatriz Bravo <beatz.bravo@gmail.com>
2024-04-01 13:21:42 -0600

    adds a check box to filter projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import projectList from "../assets/projects";
 import Project from '../components/Project'
 
 
 function Portfolio() {
+    const [filteredProjects, setFilteredProjects] = useState(projectList);
+    const [isChecked, setCheckbox] = useState(false)
 
+    // handle filters 
+    const handleFilter = (event) => {
+        const value = event.target.value;
+        const filtered = projectList.filter(project => project.techUsed.includes('React'));
+        !isChecked ? setFilteredProjects(filtered) : setFilteredProjects(projectList);
+    }
     return (
         <div id='portfolio'>
             <h1>Portfolio</h1>
+            <aside>
+                <div>
+                    <h5>Filter by tech used</h5>
+                </div>
+                <div className="filter-checkbox-container">
+                    <input type='checkbox' name='React' onClick={handleFilter} onChange={() => { setCheckbox(!isChecked) }} />
+                    {console.log('isChecked?', isChecked)}
+                    <label htmlFor="React">React</label>
+
+                </div>
+            </aside>
             <section className="projects row justify-content-evenly">
-                {projectList.map((project) => <Project key={project.id} {...project} />)}
+                <h2 className="text-align-left">Group Projects</h2>
+                {filteredProjects.map((project) => <Project key={project.id} {...project} />)}
             </section>
         </div>
     );
